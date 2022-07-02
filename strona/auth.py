aa2405db8f3541265c2452e948b6f0e9e1b41e82
karpiowygaj@gmail.com <karpiowygaj@gmail.com>
2022-07-03 01:13:52 +0200

    Działa: -login, logout, home, Sign-up

--- strona/auth.py
+++ strona/auth.py
@@ -1,16 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth',__name__)
 
 @auth.route('/login')
 def login():
-    return '<p>Login</p>'
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logut():
-    return '<p>Logout</p>'
+    return render_template("logout.html")
 
 @auth.route('/sign-up')
 def sign_up():
-    return '<p>Login</p>'
-
+    return render_template("sign-up.html")
