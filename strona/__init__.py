dd5db02062608666cefbad85c81871b2d9f0ece2
karpiowygaj@gmail.com <karpiowygaj@gmail.com>
2022-07-07 23:15:44 +0200

    Działa strona logowania. Nie działa wyświetlanie komunikatów z błędami

--- strona/__init__.py
+++ strona/__init__.py
@@ -2,12 +2,12 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config['Secret_KEY'] = 'Piotr'
+    app.secret_key = "Piotr"
 
     from .views import views
     from .auth import auth
 
-    app.register_blueprint(views,url_prefix='/')
+    app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
     return app
--- strona/auth.py
+++ strona/auth.py
@@ -1,11 +1,13 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
-auth = Blueprint('auth',__name__)
+auth = Blueprint('auth', __name__)
+
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     data = request.form
     return render_template("login.html", text="Logowanie pomyślne")
+
 
 @auth.route('/logout')
 def logut():
@@ -14,4 +16,22 @@ def logut():
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == "POST":
+        email = request.form.get('email')
+        firstname = request.form.get('FirstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        print(request.form)
+        if len(email) < 4:
+            flash('Email must be greater then 4 characters', category='error')
+        elif len(firstname) < 2:
+            flash('First name must be greater then 2 characters', category='error')
+        elif password1 != password2:
+            flash('Passwords must math', category='')
+        elif len(password1) < 7:
+            flash('Password is to weak', category='error')
+        else:
+            # add user to database
+            flash('Account created', category='success')
+
     return render_template("sign-up.html")
