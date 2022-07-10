f3d8dfbedaed5ee7e7f386f2ec73dd700f695b9b
karpiowygaj@gmail.com <karpiowygaj@gmail.com>
2022-07-11 00:56:12 +0200

    Praca nad dodawaniem notatek dla użytkownika

--- strona/views.py
+++ strona/views.py
@@ -1,10 +1,14 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
+from .models import Note
 
 views = Blueprint('views',__name__)
 
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
+    if request.method == 'POST':
+        note = request.form.get('note')
+        new_note = Note()
     return render_template("home.html", user=current_user)
