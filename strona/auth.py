33471bd2f7ae8943bd9b4fe6cf1042c94134e7c4
karpiowygaj@gmail.com <karpiowygaj@gmail.com>
2022-07-10 21:12:31 +0200

    Dodano Login-Manager

--- strona/auth.py
+++ strona/auth.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User, db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, login_user, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -15,6 +16,8 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in sucessfully', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password, try again.', category='error')
         else:
@@ -24,8 +27,10 @@ def login():
 
 
 @auth.route('/logout')
+@login_required
 def logut():
-    return render_template("logout.html")
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
@@ -53,6 +58,7 @@ def sign_up():
                             password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
+            login_user(user, remember=True)
             flash('Account created', category='success')
             return redirect(url_for('views.home'))
 
