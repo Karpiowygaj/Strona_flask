55e5b674ea16e012d329827429142a97e3534cd3
karpiowygaj@gmail.com <karpiowygaj@gmail.com>
2022-07-10 15:43:03 +0200

    Działa logowanie

--- strona/auth.py
+++ strona/auth.py
@@ -7,7 +7,19 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password1')
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in sucessfully', category='success')
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
     return render_template("login.html", text="Logowanie pomyślne")
 
 
@@ -23,8 +35,11 @@ def sign_up():
         firstname = request.form.get('FirstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-        print(request.form)
-        if len(email) < 4:
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('User already exists', category='error')
+        elif len(email) < 4:
             flash('Email must be greater then 4 characters', category='error')
         elif len(firstname) < 2:
             flash('First name must be greater then 2 characters', category='error')
