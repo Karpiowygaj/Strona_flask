1b49a00afc9ae29e17891f75f40b308b137008e2
karpiowygaj@gmail.com <karpiowygaj@gmail.com>
2022-07-11 23:01:56 +0200

    Skończone

--- strona/views.py
+++ strona/views.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
 from .models import Note
+from . import db
 
 views = Blueprint('views',__name__)
 
@@ -8,7 +9,15 @@ views = Blueprint('views',__name__)
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
+    note = request.form.get('note')
     if request.method == 'POST':
-        note = request.form.get('note')
-        new_note = Note()
+
+        if len(note) < 1:
+            flash('Note is too short', category='error')
+        else:
+            new_note = Note(notatka=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added!', category='success')
+
     return render_template("home.html", user=current_user)
